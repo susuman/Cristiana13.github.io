4e8ca38e5e0e016fb5d1a885cd4b2fbc08b8bb95
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-12 23:55:05 +0200

    updateprogram.js
    am modificat program.js

--- program.js
+++ program.js
@@ -1,14 +1,14 @@
-var cubadn = false;
+var pisica = false;
 function modifica ()
 {
 document.getElementById("id_salut").innerHTML = "Hello";
   
-  if (cubadn==false){
-    document.getElementById("id_img").src = "cercadn.jpg";
-  cubadn=true;
+  if (pisica==false){
+    document.getElementById("id_img").src = "dog.jpg";
+  pisica=true;
 }
 else{
-   document.getElementById("id_img").src = "cubadn.jpg";
-   cubadn=false;
+   document.getElementById("id_img").src = "cat.jpg";
+   pisica=false;
 }
 }
