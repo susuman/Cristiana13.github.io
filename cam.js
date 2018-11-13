37d0cd3437498ec12e73365f8f61c8adf96b6b3d
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-13 19:23:07 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,5 +1,7 @@
 document.getElementById("id_logic_version").innerHTML="Logic version 2018.11.13.0";
-
+document.getElementById("id_logic_video").addEventListener("mousedown", on_touch);
+document.getElementById("id_logic_video").addEventListener("touchstart", on_touch);
+//------------------------------------------------
 function on_ok(stream)
 {
 document.getElementById("id_video").srcObject=stream;
@@ -16,3 +18,13 @@ var p={audio:true, video:true};
 navigator.mediaDevices.getUserMedia(p).then(on_ok).catch(on_error);
 }
 //---------------------------------------------
+function on_touch (e)
+{
+ var canvas =document.getElementById("id_canvas") ;
+  var context =document.getContext("2d");
+  
+  var video=document.getElementById("id_video");
+  
+  context.drawImage(video, 0, 0);
+  
+}
