db85aec5405826c14765d09268749ef7e4a83bc5
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-13 18:26:22 +0200

    Create cam.js

---
+++ cam.js
@@ -0,0 +1,18 @@
+document.getElementById("id_logic_version").innerHTML="Logic version 2018.11.13.0";
+
+function on_ok(strem)
+{
+document.getElementById("id_video").srcObject=stream;
+}
+//----------------------------------------------
+function on_error(e)
+{
+console.log("eroare camera");
+alert("eroare camera")
+}
+function start()
+{
+var p={audio:true, video:true};
+navigator.mediaDevices.getUserMedia(p).then(on_ok).catch(on_error);
+}
+//---------------------------------------------
