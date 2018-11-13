552c813366f9aeee2fb0e9e876698c64915c6557
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-13 16:34:56 +0200

    Update ADN orientation.js

--- ADN orientation.js
+++ ADN orientation.js
@@ -1,1 +1,14 @@
-
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.2";
+ window.addEventListener("deviceorientation", la_schimbare_gyro);
+ function la_schimbare_gyro (e)
+{
+document .getElementById("id_x").innerHTMLL= e.beta;
+document .getElementById("id_y").innerHTMLL= e.gamma;
+document .getElementById("id_z").innerHTMLL= e.alpha;
+ var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+   context.beginPath();
+  context.arc(canvas.width /2, canvas.height/2,30,0,2* Math.PI);
+  context.stroke();
+  
+}
