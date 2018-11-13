11ac10e4ef348c2b846609e3974be4198b5d155f
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-13 18:42:38 +0200

    fix

--- cam.js
+++ cam.js
@@ -1,6 +1,6 @@
 document.getElementById("id_logic_version").innerHTML="Logic version 2018.11.13.0";
 
-function on_ok(strem)
+function on_ok(stream)
 {
 document.getElementById("id_video").srcObject=stream;
 }
