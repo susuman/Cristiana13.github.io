b1e6dedcc2866778c1a2794be901bb8fc31113fb
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-20 18:50:55 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,23 +1,37 @@
+document.getElementById("id_logic_version").innerHTML = 
+		"Logic version = 2018.11.20.3";
+		
+var canvas = document.getElementById("id_canvas");
+canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchmove", on_touch_move);
 
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.20.4";
-
-var canvas = document.getElementBy.Id("id_canvas")
-canvas.addEventListener("touchstart",on touch);
-var rect=canvas.getBoundingClientRect() ;
-
+var rect = canvas.getBoundingClientRect();
+//---------------------------------
 function on_touch(e)
-
-
-e.changedTouches.length
 {
-for(var i=0; i< e.changedTouches.length; i++)}
-var context = canvas.get.Context("2d");
-context.beginPath();
-context.arc(e.changedTouches.itm(i).pageX - rect.left,
-           (e.changedTouches.itm(i).pageY - rect.top,
-	20,
-	0,22* Math.PI
-	);
-context.stroke();
+	for (var i = 0; i < e.changedTouches.length; i++){
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouches.item(i).pageX - rect.left,
+					e.changedTouches.item(i).pageY - rect.top,
+					20,
+					0, 2 * Math.PI
+					);
+		context.stroke();			
+	}
 }
+//---------------------------------
+function on_touch_move(e)
+{
+	for (var i = 0; i < e.changedTouches.length; i++){
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouches.item(i).pageX - rect.left,
+					e.changedTouches.item(i).pageY - rect.top,
+					20,
+					0, 2 * Math.PI
+					);
+		context.stroke();			
+	}	
 }
+//---------------------------------
