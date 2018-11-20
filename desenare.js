33dd0b2a27154c460a770135e6eeb22dd6d9a634
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2018-11-20 18:23:47 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,1 +1,22 @@
 
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.20.0";
+
+var canvas = document.getElementBy.Id("id_canvas")
+canvas.addEventListener("touchstart",on touch);
+
+function on_touch(e)
+
+
+e.changedTouches.length
+{
+for(var i=0; i< e.changedTouches.length; i++)}
+var context = canvas.get.Context("2d");
+context.beginPath();
+context.arc(e.changedTouches.itm(i).pageX,
+           (e.changedTouches.itm(i).pageY,
+	20,
+	0,22* Math.PI
+	);
+context.stroke();
+}
+}
