e05076d34f05b39378491e8cf16d36cb373d71de
Cristiana13 <44980240+Cristiana13@users.noreply.github.com>
2019-02-09 16:29:42 +0200

    Update desenare.js
    fix

--- telefonsimplu/desenare.js
+++ telefonsimplu/desenare.js
@@ -1,54 +1,57 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.02.11.0";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.0";
 
 var svg = document.getElementById("id_svg");
-var zxc = document.getElementById("anime");
-var sound = document.getElementById("audio");
-var sound_c = document.getElementById("audio_c");
+var site_html = document.getElementById("id_html");
+var site_java = document.getElementById("id_java");
+var DBS = document.getElementById("id_DBS");
 
-svg.addEventListener("touchstart", on_touch_svg);
-svg.addEventListener("mousedown", on_touch_svg);
-//svg.addEventListener("touchend", on_touch_end);
+site_html.addEventListener("touchstart", on_touch_one);
+site_java.addEventListener("touchstart", on_touch_two);
+DBS.addEventListener("touchstart", on_touch_tree);
 
 var svg_rect = svg.getBoundingClientRect();
-var nr = 0;
 
-
-function genereaza_culoare()
+function on_touch_one(e)
 {
-	var sir = "#";
-	var litere = "0123456789ABCDEF";
-	for (var i = 0; i < 6; i++)
-		sir += litere[Math.ceil(Math.random() * 16)];
-	return sir;
-}
-
-function on_touch_svg(e)
-{
-	e.preventDefault();
-	var color = genereaza_culoare();
-	
-	for (var i = 0; i < e.changedTouches.length; i++)
-	{		
-		
-		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");	
-		cub.setAttribute("x", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
-		cub.setAttribute("y", e.changedTouches[i].pageY - svg_rect.top);
-		cub.setAttribute("width", 120);
-		cub.setAttribute("height", 50);
-		cub.setAttribute("fill", color);
-		svg.appendChild(cub);  // adaugare in svg ca si "copil" (subelement)
-
+	if(confirm("esti sigur ca vrei sa accesezi aceasta pagina??"))
+	{
+		alert("ai apasat OK");
 	}
-	nr++
-if(color == "#0f0f00")
-{
-	nr++
+	else
+	{
+		alert("ai apasat anulare");
+	}
+		
+window.open("https://www.w3schools.com");
+alert("ai accesat pagina html5");
 }
-if(nr == 15)
+
+function on_touch_two(e)
 {
-	alert("felicitari ai apasat de 1000000000000 ori si ai gasit 10 euro");
-	sound_c.play();
+		if(confirm("esti sigur ca vrei sa accesezi aceasta pagina??"))
+	{
+		alert("ai apasat OK");
+	}
+	else
+	{
+		alert("ai apasat anulare");
+	}
+	
+window.open("https://www.java.com/en/");
+alert("ai accesat pagina javascript");
 }
-	  	
-sound.play();
-}		  
+
+function on_touch_tree(e)
+{
+		if(confirm("esti sigur ca vrei sa accesezi aceasta pagina??"))
+	{
+		alert("ai apasat OK");
+	}
+	else
+	{
+		alert("ai apasat anulare");
+	}
+	
+window.open("https://screenrant.com/tag/dragonball/");
+alert("ai accesat pagina dragon ball");
+}
